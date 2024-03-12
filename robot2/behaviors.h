c8c3cad5a5c4ae599754335e52000066e0c5d9e3
Garrett Jones <jones.garrett_m@outlook.com>
2024-03-12 15:50:07 -0400

    Add behavior.cpp

---
+++ robot2/behaviors.h
@@ -0,0 +1,60 @@
+#ifndef IGVC2024_BEHAVIORS_402732
+#define IGVC2024_BEHAVIORS_402732
+
+#include "robot.h"
+
+class Behavior {
+public:
+    Vehicle& vehicle;
+
+    Behavior(Vehicle& v) : vehicle(v) {}
+
+    int run();
+};
+
+class FollowLine : public Behavior {
+    using Behavior::Behavior;
+
+public:
+    int run();
+
+    enum Result { SUCCESS, LOST_LINE };
+};
+
+class ForwardOnLine : public Behavior {
+    using Behavior::Behavior;
+
+public:
+    int run();
+
+    enum Result { SUCCESS, LOST_LINE, LEFT_OF_LINE, RIGHT_OF_LINE };
+};
+
+class LeftTowardLine : public Behavior {
+    using Behavior::Behavior;
+
+public:
+    int run();
+
+    enum Result { SUCCESS, LOST_LINE, LEFT_OF_LINE, RIGHT_OF_LINE };
+};
+
+class RightTowardLine : public Behavior {
+    using Behavior::Behavior;
+
+public:
+    int run();
+
+    enum Result { SUCCESS, LOST_LINE, LEFT_OF_LINE, RIGHT_OF_LINE };
+};
+
+class TestMovement : public Behavior {
+    using Behavior::Behavior;
+
+public:
+    int run();
+
+    enum Result { SUCCESS };
+};
+
+#endif
